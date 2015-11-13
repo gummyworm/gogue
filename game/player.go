21395e41d0ebba407a636ae2111e7eb9d98b2118
Bryce <brycemwilson@gmail.com>
2015-11-13 08:35:16 -0600

    Clean up files

--- game/player.go
+++ game/player.go
@@ -4,25 +4,25 @@ type Player struct {
 	selection []interface{}
 }
 
-func (p *Player) Cmd(action Rune, target interface{}) {
-	for _, u := range(player.selection) {
-	switch(action) {
+func (p *Player) Cmd(action rune, target interface{}) {
+	for _, u := range p.selection {
+		switch action {
 		case 'l':
 			l, ok := u.(Seer)
 			if ok {
-				l.See()
+				l.See(target)
 			}
 		case 'a':
 			a, ok := u.(Attacker)
 			if ok {
-				a.Attack()
+				a.Attack(target)
 			}
 		case 'q':
 			q, ok := u.(Quaffer)
 			if ok {
-				q.Quaff()
+				q.Quaff(target)
 			}
 		default:
+		}
 	}
 }
-
